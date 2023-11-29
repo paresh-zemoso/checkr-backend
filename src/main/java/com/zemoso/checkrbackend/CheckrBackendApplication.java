c95d72c68cfe6c07cc03ddbf8895a0cfe69c4989
paresh-zemoso <paresh.chavan@zemosolabs.com>
2023-11-29 13:16:12 +0530

    Initial commit

---
+++ src/main/java/com/zemoso/checkrbackend/CheckrBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zemoso.checkrbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckrBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckrBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zemoso/checkrbackend/CheckrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zemoso.checkrbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckrBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
